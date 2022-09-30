f16787b3423fe9a6ce74b7c5fbb41653404ba631
Arne Van Bael <arne.vanbael@axxes.com>
2022-09-30 13:00:13 +0200

    added api contract

---
+++ src/DeGierigeHollander/DeGierigeHollander.Gui/Shared/Answer.cs
@@ -0,0 +1,11 @@
+﻿namespace DeGierigeHollander.Gui.Shared;
+
+public class Answer<T>
+{
+    public T Value { get; set; }
+}
+
+public class AnswerResponse
+{
+    public string ResponseText { get; set; }
+}
---
+++ src/DeGierigeHollander/DeGierigeHollander.Gui/Shared/Question.cs
@@ -0,0 +1,8 @@
+﻿namespace DeGierigeHollander.Gui.Shared;
+
+public class QuestionBase
+{
+    public string QuestionString { get; set; } = string.Empty;
+    public string ResponseUrl { get; set; } = string.Empty;
+    public Type AnswerType { get; set; }
+}
